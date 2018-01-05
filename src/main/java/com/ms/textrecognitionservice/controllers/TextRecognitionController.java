378319f377ad6723fee2a8bd59f8ecd9a9a777a6
12masta <stanek.marcinp@gmail.com>
2018-01-05 02:33:31 +0100

    TextRecognitionController update to handle new case with passing images as base64 string

--- src/main/java/com/ms/textrecognitionservice/controllers/TextRecognitionController.java
+++ src/main/java/com/ms/textrecognitionservice/controllers/TextRecognitionController.java
@@ -1,22 +1,26 @@
 package com.ms.textrecognitionservice.controllers;
 
+import com.ms.textrecognitionservice.models.ImageModel;
 import com.ms.textrecognitionservice.models.RecipeModel;
 import com.ms.textrecognitionservice.utils.imagerecognize.GoogleImageRecognitionServiceImpl;
 import com.ms.textrecognitionservice.utils.imagerecognize.ImageRecognitionService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 @RestController
+@RequestMapping("/recognizeTest")
 public class TextRecognitionController {
 
-    @RequestMapping("/recognizeTest")
-    RecipeModel recognizeText() {
+    @RequestMapping(method = RequestMethod.POST)
+    RecipeModel recognizeText(@RequestBody ImageModel imageModel) {
 
         ImageRecognitionService imageRecognitionService = new GoogleImageRecognitionServiceImpl();
         try {
-            return imageRecognitionService.recognizeText();
+            return imageRecognitionService.recognizeText(imageModel.getImageInBase64());
         } catch (Exception e) {
             e.printStackTrace();
         }
